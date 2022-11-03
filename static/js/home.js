b4460a26ed455dc92ebc9a5dd8bbd61eafc01eea
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-11-03 16:52:01 +0000

    chatbot message display and disappear

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,18 @@
 /**TRY CODING HERE TO MAKE BACKROUND TEXT DISAPPEAR WHEN CLICKING
  * ON THE CHECLIST (3 LINES)
  */
+
+
+
+
+//TOGGLE CHATBOT
+const chatbotToggle = document.getElementsByClassName("chatbot-message-wrapper")
+
+function toggleONOFF () {
+    //if the toggle is not showing the chatbox then 
+    //display it, else not.
+    if (chatbotToggle[0].style.display == "none")
+        chatbotToggle[0].style.display = "block"
+    else
+        chatbotToggle[0].style.display = "none"
+}
