2684281410a698d9a7497490efbe1320662db8cb
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-11-04 14:03:53 +0000

    register created

---
+++ static/js/register.js
@@ -0,0 +1,11 @@
+//This is for the popup that will appear when you click the Sign Up | Login
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
