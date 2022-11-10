f6164d03bb3d6790e8bf10be9e35b8b5cda4685f
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-11-10 13:50:20 +0000

    added sell & buy code to .py file

--- start.py
+++ start.py
@@ -10,10 +10,23 @@ from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
+# home page
 @app.route('/home/')
 def home():
     return render_template('home.html')
 
+
+# sell page. 
+# this will open a new tab
+@app.route('/sell/')
+def sell():
+    return render_template('sell.html')
+
+# buy page. 
+# this will open a new tab
+@app.route('/buy/')
+def buy():
+    return render_template('buy.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
