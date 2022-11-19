14e5cc2ee717f0d625e45e0d853df522ab36362b
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-11-19 21:47:25 +0000

    fixes with url

--- start.py
+++ start.py
@@ -41,7 +41,7 @@ def buy():
 # (in the section of modal), it will link to the html files and open them 
 # on the browser.
 @app.route('/register/')
-def register():
+def modal():
     return render_template('modal.html')
 
 if __name__ == "__main__":
