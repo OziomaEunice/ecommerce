143e0cd3067fe6e2a3ea22f80f77c6a69f35ddde
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-11-22 15:06:51 +0000

    profile display and menu

---
+++ static/js/home2.js
@@ -0,0 +1,7 @@
+//define the variable
+let subMenu = document.getElementById("subMenu");
+
+function openMenu(){
+    subMenu.classList.toggle("open-menu")
+    //when the profile display is clicked, the menu appears and vice-versa
+}
