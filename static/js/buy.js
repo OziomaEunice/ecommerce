a06a60c1c5766b3f5e757177ccad8cf40599cfb7
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-11-29 20:47:53 +0000

    buy page

---
+++ static/js/buy.js
@@ -0,0 +1,7 @@
+/*This is for the first section that will have a typing animation*/
+var typed = new Typed(".auto-type", {
+    strings: ["Look out for new items", "Mark items with a &#11088;", "Find out more."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
