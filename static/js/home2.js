874e1de726aef018af6b48eeee7c1d3395a9c304
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-11-30 11:47:34 +0000

    logged page with preloader

--- static/js/home2.js
+++ static/js/home2.js
@@ -1,3 +1,21 @@
+/*This is for the preloader*/
+//define the loader variable
+var loader = document.getElementById("preloader");
+
+//create an event function
+window.addEventListener("load", function(){
+    //hide the loader
+    loader.style.display = "none";
+
+    /**The loader works, however, it disappears very quickly
+     * when refreshing it, so what can be done is to have 
+     * a video link (see profile.html for tag iframe) and
+     * paste it multiple times
+     */
+})
+
+
+/*Sub-menu in nav-bar*/
 //define the variable
 let subMenu = document.getElementById("subMenu");
 
